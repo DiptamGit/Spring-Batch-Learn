bff9c82e61f4f071f29ba509a4193fbce461901c
diptamsarkar@gmail.com <dipgit17>
2019-12-23 19:11:04 -0600

    <<First Commit>>

---
+++ src/main/java/com/diptam/SpringBatchLearn/SpringBatchLearnApplication.java
@@ -0,0 +1,13 @@
+package com.diptam.SpringBatchLearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchLearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diptam/SpringBatchLearn/SpringBatchLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diptam.SpringBatchLearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
